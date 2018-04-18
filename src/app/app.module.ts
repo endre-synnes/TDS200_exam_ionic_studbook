0ac95d7cdc64eb99df8856c5398f9585f1d781d1
synend16 <synend16@student.westerdals.no>
2018-04-18 23:10:26 +0200

    added correct location provider

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,6 +6,7 @@ import { StatusBar } from '@ionic-native/status-bar';
 
 import { MyApp } from './app.component';
 import { HomePage } from '../pages/home/home';
+import { LocationProvider } from '../providers/location/location';
 
 @NgModule({
   declarations: [
@@ -24,7 +25,8 @@ import { HomePage } from '../pages/home/home';
   providers: [
     StatusBar,
     SplashScreen,
-    {provide: ErrorHandler, useClass: IonicErrorHandler}
+    {provide: ErrorHandler, useClass: IonicErrorHandler},
+    LocationProvider
   ]
 })
 export class AppModule {}
--- src/providers/location/location.ts
+++ src/providers/location/location.ts
@@ -2,16 +2,16 @@ import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 
 /*
-  Generated class for the LocationProvierProvider provider.
+  Generated class for the LocationProvider provider.
 
   See https://angular.io/guide/dependency-injection for more info on providers
   and Angular DI.
 */
 @Injectable()
-export class LocationProvierProvider {
+export class LocationProvider {
 
   constructor(public http: HttpClient) {
-    console.log('Hello LocationProvierProvider Provider');
+    console.log('Hello LocationProvider Provider');
   }
 
 }
