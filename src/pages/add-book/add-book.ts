2edab2238a88eac54233a3d02017619fef3e8881
synend16 <synend16@student.westerdals.no>
2018-04-21 00:10:52 +0200

    Almost working to add pictures still bug

--- src/pages/add-book/add-book.ts
+++ src/pages/add-book/add-book.ts
@@ -1,5 +1,5 @@
 import { Component } from '@angular/core';
-import { IonicPage, NavController, NavParams } from 'ionic-angular';
+import {IonicPage, NavController, NavParams, normalizeURL, Platform} from 'ionic-angular';
 import {Camera} from "@ionic-native/camera";
 import {LocationProvider} from "../../providers/location/location";
 import {AngularFirestore} from "angularfire2/firestore";
@@ -18,12 +18,14 @@ export class AddBookPage {
 
   private book: Book = {
     author:"",
-    price:150,
+    price:0,
     seller:"",
     sold:false,
     title:"",
     imgUrl:"",
     city:"Oslo"};
+
+  private imageString: string = "";
 
 
   constructor(public navCtrl: NavController,
@@ -33,14 +35,55 @@ export class AddBookPage {
               private locationProvider: LocationProvider,
               private af: AngularFirestore,
               private afStorage: AngularFireStorage,
-              private bookProvider: BooksProvider) {
+              private bookProvider: BooksProvider,
+              private platform: Platform) {
 
   }
 
 
   addBook(){
-    this.book.seller = this.getCurrentUser();
-    this.bookProvider.addBook(this.book);
+    if (this.imageString !== "") {
+      this.book.seller = this.getCurrentUser();
+
+      let imgFileName = `${this.book.seller}_${new Date().getTime()}.png`;
+
+      let task = this.afStorage
+        .ref(imgFileName)
+        .putString(this.imageString, 'base64', {contentType: 'image/png'});
+
+      let uploadEvent = task.downloadURL();
+
+      uploadEvent.subscribe((uploadImageUrl) => {
+        this.book.imgUrl = uploadImageUrl;
+        this.bookProvider.addBook(this.book)
+          .then(() => this.navCtrl.pop())
+          .catch(console.log)
+      });
+    }
+
+  }
+
+  takePicture(){
+    this.camera.getPicture({
+      destinationType: this.platform.is('ios') ? this.camera.DestinationType.FILE_URI : this.camera.DestinationType.DATA_URL,
+      encodingType: this.camera.EncodingType.JPEG,
+      cameraDirection: this.camera.Direction.BACK,
+      correctOrientation: true,
+      mediaType: this.camera.MediaType.PICTURE
+    })
+      .then( (imageData) => {
+
+        let base64Image = null;
+
+        //get photo from the camera based on platform type
+        if (this.platform.is('ios'))
+          base64Image = normalizeURL(imageData);
+        else
+          base64Image = "data:image/jpeg;base64," + imageData;
+
+        this.imageString = base64Image;
+
+      }).catch(console.log)
   }
 
 
